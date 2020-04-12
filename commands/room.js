6a83ecc9da3c31c4fdf3a94e3d5d7f5de15c70d2
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2020-04-12 15:11:22 +0200

    Added start of bot + room command
    Very messy, needs a lot of cleanup

---
+++ commands/room.js
@@ -0,0 +1,104 @@
+module.exports = {
+  name: 'room',
+  description: 'Create and manage your private room',
+  async execute(msg, args, keyv) {
+    let subcommand = args[0];
+    if (args.length === 0) {
+      msg.reply('your own private room where you can invite other people');
+    } else if (subcommand === 'create') {
+      let rooms;
+      let rooms_id = await keyv.get('rooms_category_id');
+      if (rooms_id) {
+        rooms = await msg.client.channels.fetch(rooms_id);
+      } else {
+        rooms = await msg.guild.channels.create('rooms', { type: 'category' });
+        keyv.set('rooms_category_id', rooms.id);
+      }
+      let room_id = await keyv.get('room-' + msg.author.id);
+      if (room_id) {
+        msg.reply('you already have a room');
+      } else {
+        let room = await msg.guild.channels.create(msg.author.username + '\'s room', {
+          type: 'voice', bitrate: 96000, parent: rooms, permissionOverwrites: [
+            {
+              id: '698545799839219822', // @everyone
+              deny: ['CONNECT']
+            }
+          ]
+        });
+        keyv.set('room-' + msg.author.id, room.id);
+        msg.reply('I have created your private room \nInvite someone with `/room invite @user`')
+      }
+    } else if (subcommand === 'delete') {
+      let room_id = await keyv.get('room-' + msg.author.id);
+      if (room_id) {
+        let room = await msg.client.channels.fetch(room_id);
+        room.delete();
+        keyv.delete('room-' + msg.author.id);
+        msg.reply('your room has been deleted');
+      } else {
+        msg.reply('you don\'t have a room');
+      }
+    } else if (subcommand === "rename") {
+      let room_id = await keyv.get('room-' + msg.author.id);
+      if (room_id) {
+        let room = await msg.client.channels.fetch(room_id);
+        room.setName(args.slice(1).join(' '));
+      } else {
+        msg.reply('you don\'t have a room');
+      }
+    } else if (subcommand === 'invite') {
+      let room_id = await keyv.get('room-' + msg.author.id);
+      if (room_id) {
+        let room = await msg.client.channels.fetch(room_id);
+        let user = getUserFromMention(msg, args[1]);
+        if (user) {
+          room.createOverwrite(user, {
+            CONNECT: true
+          });
+          user.send('You now have access to <@' + msg.author.id + '>\'s room');
+          msg.reply(user.username + ' now has access to join your room');
+        } else {
+          msg.reply('not a valid user');
+        }
+      } else {
+        msg.reply('you don\'t have a room');
+      }
+    } else if (subcommand === "kick") {
+      let room_id = await keyv.get('room-' + msg.author.id);
+      if (room_id) {
+        let room = await msg.client.channels.fetch(room_id);
+        let user = getUserFromMention(msg, args[1]);
+        if (user) {
+          room.permissionOverwrites.get(user.id).delete();
+          console.log(user);
+          msg.guild.member(user).voice.kick();
+          user.send('You have been kicked from <@' + msg.author.id + '>\'s room');
+          msg.reply(user.username + ' has been kicked from your room');
+        } else {
+          msg.reply('not a valid user');
+        }
+      } else {
+        msg.reply('you don\'t have a room');
+      }
+    } else {
+      msg.reply('unknown command arguments');
+    }
+  }
+}
+
+function getUserFromMention(msg, mention) {
+  if (!mention) return;
+
+  if (mention.startsWith('<@') && mention.endsWith('>')) {
+    mention = mention.slice(2, -1);
+
+    if (mention.startsWith('!')) {
+      mention = mention.slice(1);
+    }
+
+    return msg.client.users.cache.get(mention);
+  } else {
+    return;
+  }
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const Keyv = require('keyv');
+
+// Initialize Discord client
+const client = new Discord.Client();
+
+// Get all commands from folder
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Connect to redis
+const keyv = new Keyv('redis://192.168.0.100:6379', { namespace: 'alien-network' });
+keyv.on('error', e => console.error('Keyv connection error:', e));
+
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity('everybody 😶', { type: 'WATCHING' });
+})
+
+// Capture client messages
+client.on('message', async msg => {
+  if (!msg.content.startsWith('/') || msg.author.bot) return;
+  console.log(`${msg.author.username}: ${msg.content}`);
+  const args = msg.content.slice('/'.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command === 'room') {
+    client.commands.get('room').execute(msg, args, keyv);
+  } else {
+    msg.reply('unkown command');
+  }
+})
+
+// Get discord token from environment variable
+DISCORD_TOKEN = process.env.DISCORD_TOKEN
+
+if (DISCORD_TOKEN) {
+  client.login();
+} else {
+  console.log('Please set the DISCORD_TOKEN environment variable');
+}

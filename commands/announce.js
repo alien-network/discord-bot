2c4122472e7ba3eea5571378f0663e6a319cab9d
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2020-05-06 20:55:55 +0200

    Added announce edit + cleanup

--- commands/announce.js
+++ commands/announce.js
@@ -4,14 +4,27 @@ const { admin_user_id, announcements_channel_id } = require('../config.json');
 module.exports = {
   name: 'announce',
   description: 'Write a announcement',
-  usage: '- `/announce message <message>` Place an announcement in the <#' + announcements_channel_id + '> channel \n- `/announce command <command>` Announce a new command',
+  usage: '- `/announce message <message>` Place an announcement in the <#' + announcements_channel_id + '> channel \n- `/announce command <command>` Announce a new command \n- `/announce edit <message_id> [message/command] <message/command>` Edit an announcement',
+  subcommands: ['message', 'command', 'edit'],
   async execute(msg, args, commands) {
+    // Check if user is admin
     if (msg.author.id !== admin_user_id) return;
+
+    // No arguments
+    if (args.length === 0) return;
+
+    // Get the subcommand ex: create, delete, invite, ...
+    let subcommand = args[0];
+    if (!this.subcommands.includes(subcommand)) {
+      msg.reply('Unknown command option');
+      return;
+    };
+
     let announcements_channel = await msg.client.channels.fetch(announcements_channel_id);
     if (args[0] === 'message') {
       announcements_channel.send(args.slice(1).join(' '));
     } else if (args[0] === 'command') {
-      command = commands.get(args[1]);
+      const command = commands.get(args[1]);
       if (command) {
         const embed = new MessageEmbed()
           .setTitle('✨ New bot command: `/' + command.name + '`')
@@ -23,6 +36,32 @@ module.exports = {
       } else {
         msg.reply('Command not found');
       }
+    } else if (args[0] === 'edit') {
+      const message_id = args[1];
+      const messages = await announcements_channel.messages.fetch({ around: message_id, limit: 1 })
+      const message = messages.first()
+      if (!message) {
+        msg.reply('Message not found');
+        return;
+      }
+      if (args[2] === 'message') {
+        message.edit(args.slice(3).join(' '))
+      } else if (args[2] === 'command') {
+        const command = commands.get(args[3]);
+        if (!command) {
+          msg.reply(`Command ${args[3]} not found`);
+          return;
+        }
+        const embed = new MessageEmbed()
+          .setTitle('✨ New bot command: `/' + command.name + '`')
+          .setColor(0x370052)
+          .setFooter('Brought to you by ' + msg.author.username, msg.author.avatarURL())
+          .setDescription(command.description)
+          .addField('Usage: ', command.usage);
+        message.edit(embed);
+      } else {
+        msg.reply('Unkown command option');
+      }
     }
-  }   
+  }
 }
--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
   name: 'help',
   description: 'Get more info about other commands',
-  usage: '- `/help command` Get more info about "command"',
+  usage: '- `/help <command>` Get more info about a specific command',
   async execute(msg, args, commands) {
     let commands_text = '';
     commands.forEach(element => {
@@ -13,11 +13,15 @@ module.exports = {
       const embed = new MessageEmbed()
         .setTitle('Help')
         .setColor(0x370052)
-        .setDescription('To get more information about a certain command use `/help command`')
+        .setDescription('To get more information about a certain command use `/help <command>`')
         .addField('Commands: ', commands_text);
       msg.reply(embed);
     } else {
       command = commands.get(args[0]);
+      if (!command) {
+        msg.reply('Unknown command');
+        return;
+      };
       const embed = new MessageEmbed()
         .setTitle('Command: `/' + command.name + '`')
         .setColor(0x370052)

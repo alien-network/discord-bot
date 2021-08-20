343fca3958d2703fa34370b033ab15981fed9062
jarnedemeulemeester <jarnedemeulemeester@gmail.com>
2021-08-20 14:51:21 +0200

    Add register-commands script to register slash commands

---
+++ register-commands.js
@@ -0,0 +1,23 @@
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { commandsArray } from './commands/index.js';
+import config from './config.js';
+
+const { DISCORD_TOKEN } = process.env;
+
+const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+    console.log(commandsArray);
+
+    await rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), {
+      body: commandsArray,
+    });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
